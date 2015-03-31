953d4ca6f2cba8a699862b90c549b3c115d1e488
monica <tangerinetan@126.com>
2015-03-31 11:54:56 +0800

    waterfall

---
+++ js/waterfall-auto.js
@@ -0,0 +1,31 @@
+$( window ).on( "load", function(){
+    waterfall();
+});
+
+function waterfall(){
+    var dw=$(document).width();
+        var imgW = Math.floor(dw*0.48);
+        $('.pic-module').width(imgW);
+        $('.box-module').css('padding',Math.floor(dw*0.01));
+        
+    var $boxs = $('#main-module > div');
+    var w = $boxs.eq(0).outerWidth();
+    var cols = Math.floor($(window).width()/w);
+    $('#main-module').width(cols*w).css('margin','0 auto');
+    var hArr = [];
+    $boxs.each(function(index,value){
+        var h = $boxs.eq(index).outerHeight();
+        if(index<cols){
+            hArr[index]=h;
+        }else{
+            var minH = Math.min.apply(null,hArr);
+            var minHIndex = $.inArray(minH,hArr);
+            $(value).css({
+                'position':'absolute',
+                'top':minH+'px',
+                'left':minHIndex*w+'px'
+            })
+            hArr[minHIndex]+=$boxs.eq(index).outerHeight();
+        }
+    });
+};
